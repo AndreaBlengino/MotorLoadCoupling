541f04dbe885714954d491b5ed7bd6c926a24a0e
Andrea Blengino <ing.andrea.blengino@gmail.com>
2021-05-05 21:36:10 +0200

    Added header

--- MotorLoadCoupling/__init__.py
+++ MotorLoadCoupling/__init__.py
@@ -1,1 +1,9 @@
+"""
+=================
+MotorLoadCoupling
+=================
+
+Analysis of the start-up transient dynamics of a motor-gearbox-load coupling.
+"""
+
 from .MotorLoadCoupling import Coupling
